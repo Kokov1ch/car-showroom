0c9054a7b2856795366ec6ef9daf2357f0a8a061
Kokov1ch <84625375+Kokov1ch@users.noreply.github.com>
2023-09-15 19:48:35 +1000

    Update DealController.php

--- src/Controller/DealController.php
+++ src/Controller/DealController.php
@@ -20,17 +20,10 @@ class DealController extends AbstractController
      */
     public function index(BuyerRepository $buyerRepository, Request $request, ManagerRepository $managerRepository, ProductRepository $productRepository, ManagerRegistry $registry): Response
     {
-//        var_dump($request->get('product'));
         $entityManager = $registry->getManager();
-//        $curBuyer = $request->get('buyer');
-//        if($request->get('send')==NULL) {
             $fio = NULL;
-            //$managerId = NULL;
-            //$productId = NULL;
             $series = NULL;
             $number = NULL;
-//        }
-//        else{
             $fio = $request->get('fio');
             $series = $request->get('series');
             $number = $request->get('number');
@@ -40,25 +33,7 @@ class DealController extends AbstractController
         if(session_id() === "") session_start();
         if ($_SESSION['manager']==NULL) $managerId = NULL;
         else $managerId = $_SESSION['manager'];
-           // var_dump($productId);
-//        }
-//        if ($request->get('fio')!=NULL)
-//            $fio = $request->get('fio');
-//
-//        if ($request->get('series')!=NULL)
-//            $series = $request->get('series');
-//
-//        if($request->get('number')!=NULL)
-//            $number = $request->get('number');
-//
-//        if ($request->get('manager')!=NULL)
-//            $managerId = $request->get('manager');
-//
-//        if($request->get('product')!=NULL)
-//            $productId = $request->get('product');
 
-         //var_dump($managerId);
-        //if ($request->get('choose') == NULL) {
             if ($request->get('send')) {
                 if (($fio != NULL) && ($series != NULL) && ($number != NULL)) {
                     $buyer = new Buyer();
@@ -79,43 +54,23 @@ class DealController extends AbstractController
                     $entityManager->flush();
                     return $this->redirectToRoute('requests');
                 } else {
-                    //var_dump($curBuyer);
                     $buyer = $buyerRepository->find($curBuyer);
                     $requestProduct = new \App\Entity\Request();
                     $product = $productRepository->find($productId);
                     $manager = $managerRepository->find($managerId);
-//                    var_dump($product->getId());
-//                    var_dump($manager->getManagerFio());
+
                     $requestProduct->setBuyer($buyer);
                     $requestProduct->setProduct($product);
                     $requestProduct->setManager($manager);
-//                    var_dump($requestProduct->getProduct()->getId());
-//                    var_dump($requestProduct->getManager()->getId());
-//                    var_dump($requestProduct->getBuyer()->getId());
+
                     $entityManager->persist($requestProduct);
                     $entityManager->flush();
                     return $this->redirectToRoute('requests');
                 }
             }
-//            }
-       // }
-//        else
-//        {
-//            var_dump($productId);
-//            $buyer = $buyerRepository->find($curBuyer);
-//            $requestProduct = new \App\Entity\Request();
-//            $product = $productRepository->find($productId);
-//            $manager = $managerRepository->find($managerId);
-//            $requestProduct->setBuyer($buyer);
-//            $requestProduct->setProduct($product);
-//            $requestProduct->setManager($manager);
-//            $entityManager->persist($requestProduct);
-//            $entityManager->flush();
-//            return $this->redirectToRoute('requests');
-//        }
+
         return $this->render('deal/index.html.twig', [
             'buyers' => $buyerRepository->findAll(),
-            //'curBuyer' => $curBuyer,
             'product' => $productId,
             'manager' => $managerId,
         ]);
